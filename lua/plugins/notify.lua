ff9f5988453ef112521cc22d45344e84a8ff35df
Jan Pistor <pistor.jan@gmail.com>
2024-08-14 10:45:51 +0200

    refactor: move notifications to bottom right

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -8,7 +8,8 @@ return {
     },
   },
   opts = {
-    timeout = 3000,
+    timeout = 1000,
+    top_down = false,
     -- max_height = function() return math.floor(vim.o.lines * 0.75) end,
     -- max_width = function() return math.floor(vim.o.columns * 0.75) end,
     max_height = function() return 10 end,
